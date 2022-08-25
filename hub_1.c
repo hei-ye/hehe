d3c17e20337df9c18457b622bdfb3378765790d3
hei-ye <107086808+hei-ye@users.noreply.github.com>
2022-08-25 20:02:28 +0800

    Add files via upload

---
+++ hub_1.c
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int n=-1;
+//	int count=0;
+//	do
+//	{
+//		count++;
+//	}while(n=n&(n-1));//Éý¼¶°æ
+//	printf("%d\n",count);
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	int n=15;
+	int count=0,i=0;
+	for(i=0;i<32;i++)
+	{
+		if(((n>>i)&1)==1)
+			count++;
+	}
+	printf("%d\n",count);
+	return 0;
+}
